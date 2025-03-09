f516acad47afb6c685790264469a048ccf20a75a
konhito <konhito02@gmail.com>
2025-03-09 09:23:23 +0530

    updated the iets

--- src/app/ielts/page.tsx
+++ src/app/ielts/page.tsx
@@ -3,8 +3,26 @@ import Image from "next/image";
 import admiFirst from "@/public/ietsFir.jpg";
 import admiSec from "@/public/ieltsSec.jpg";
 import CallToAction from "@/components/CallToAction";
+import ietts6 from "@/public/iets6.jpg";
+import iets7 from "@/public/iets7.jpg";
+
+import { User, Users, BookOpen } from "lucide-react";
 
 export default function HomePage() {
+  const cards = [
+    {
+      icon: <User size={50} className="text-[#0B3555]" />,
+      title: "TUTORS MADRELINGUA",
+    },
+    {
+      icon: <Users size={50} className="text-[#0B3555]" />,
+      title: "BUDDY PROGRAM",
+    },
+    {
+      icon: <BookOpen size={50} className="text-[#0B3555]" />,
+      title: "MATERIALE ESCLUSIVO",
+    },
+  ];
   return (
     <main className="bg-white text-gray-800 min-h-screen flex flex-col items-center ">
       {/* Images Section */}
@@ -14,100 +32,66 @@ export default function HomePage() {
           alt="Admission Portal"
           className="rounded-lg shadow-md mb-6 mt-20"
         />
-        <section className="w-full max-w-7xl grid grid-cols-1 md:grid-cols-3 gap-8 mb-7">
-          <div className="bg-blue-100 p-6 rounded-lg shadow-md text-center">
-            <h3 className="text-xl font-semibold text-blue-600 mb-4">
-              APPROCCIO PERSONALIZZATO
-            </h3>
-            <p className="text-gray-600">
-              Corsi studiati sulle esigenze individuali.
-            </p>
+        <section className="w-full max-w-7xl grid grid-cols-1 md:grid-cols-4 gap-8 mb-8">
+          <div className="text-center">
+            <h2 className="text-3xl font-bold text-blue-600">99.3%</h2>
+            <p className="text-gray-600">SUCCESS RATE</p>
           </div>
-          <div className="bg-blue-100 p-6 rounded-lg shadow-md text-center">
-            <h3 className="text-xl font-semibold text-blue-600 mb-4">
-              NETWORK ESCLUSIVO
-            </h3>
-            <p className="text-gray-600">
-              Accesso a un network di professionisti.
-            </p>
+          <div className="text-center">
+            <h2 className="text-3xl font-bold text-blue-600">50+</h2>
+            <p className="text-gray-600">STUDENTI/ANNO</p>
           </div>
-          <div className="bg-blue-100 p-6 rounded-lg shadow-md text-center">
-            <h3 className="text-xl font-semibold text-blue-600 mb-4">
-              PIATTAFORMA ALL IN ONE
-            </h3>
-            <p className="text-gray-600">
-              Tutto ciò di cui hai bisogno in un unico posto.
-            </p>
+          <div className="text-center">
+            <h2 className="text-3xl font-bold text-blue-600">7</h2>
+            <p className="text-gray-600">ANNI DI ESPERIENZA</p>
+          </div>
+          <div className="text-center">
+            <h2 className="text-3xl font-bold text-blue-600">10,000</h2>
+            <p className="text-gray-600">ORE DI CONSULENZA</p>
           </div>
         </section>
+
+        <div className="flex justify-center rounded mb-2 items-center gap-20 p-8 bg-[#0B3555] ">
+          {cards.map((card, index) => (
+            <div
+              key={index}
+              className="w-64 h-64 bg-[#FFF3DB] rounded-lg shadow-lg flex flex-col items-center justify-center text-[#0B3555] text-center font-bold p-4"
+            >
+              {card.icon}
+              <p className="mt-4">{card.title}</p>
+            </div>
+          ))}
+        </div>
 
         <Image
           src={admiSec}
           alt="Admission Portal"
-          className="rounded-lg shadow-md"
+          className="rounded-lg shadow-md mt-2"
+        />
+        <Image
+          src={ietts6}
+          alt="Admission Portal"
+          className="rounded-lg shadow-md my-4"
+        />
+        <Image
+          src={iets7}
+          alt="Admission Portal"
+          className="rounded-lg shadow-md my-4"
         />
       </div>
+      <div className="flex flex-col items-center justify-center my-2 mb-20 bg-white">
+        {/* Title */}
+        <h1 className="text-3xl font-bold text-[#153B5C] mb-6">
+          Incontra i nostri Tutors
+        </h1>
 
-      {/* Services Section */}
-      <section className="w-full max-w-7xl grid grid-cols-1 md:grid-cols-4 gap-8 mb-16">
-        <div className="text-center">
-          <h2 className="text-3xl font-bold text-blue-600">99.3%</h2>
-          <p className="text-gray-600">SUCCESS RATE</p>
-        </div>
-        <div className="text-center">
-          <h2 className="text-3xl font-bold text-blue-600">50+</h2>
-          <p className="text-gray-600">STUDENTI/ANNO</p>
-        </div>
-        <div className="text-center">
-          <h2 className="text-3xl font-bold text-blue-600">7</h2>
-          <p className="text-gray-600">ANNI DI ESPERIENZA</p>
-        </div>
-        <div className="text-center">
-          <h2 className="text-3xl font-bold text-blue-600">10,000</h2>
-          <p className="text-gray-600">ORE DI CONSULENZA</p>
-        </div>
-      </section>
+        {/* Button */}
+        <button className="bg-[#FF5252] text-black text-lg font-light px-8 py-4 rounded-lg">
+          vedi tutoring gmat
+        </button>
+      </div>
 
-      <section className="mb-12">
-        <h2 className="text-2xl font-bold flex items-center justify-center text-blue-600 mb-6">
-          Il nostro approccio
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <div className="bg-blue-100 p-6 rounded-lg text-center">
-            <h3 className="text-xl font-bold text-blue-600">
-              RAFFORZIAMO LE COMPETENZE
-            </h3>
-          </div>
-          <div className="bg-blue-100 p-6 rounded-lg text-center">
-            <h3 className="text-xl font-bold text-blue-600">
-              SIMULIAMO SITUAZIONI REALI
-            </h3>
-          </div>
-          <div className="bg-blue-100 p-6 rounded-lg text-center">
-            <h3 className="text-xl font-bold text-blue-600">
-              OTTIMIZZIAMO LA TECNICA
-            </h3>
-          </div>
-        </div>
-      </section>
-
-      <section className="mb-12">
-        <h2 className="text-2xl font-bold text-blue-600 mb-6">
-          Corsi di gruppo
-        </h2>
-        <p className="mb-4">
-          Pratica le tue skills con discussioni e attività di gruppo (c.a. 10
-          persone).
-        </p>
-        <ul className="list-disc list-inside">
-          <li>
-            Scegli un corso intensivo di 2 mesi o di allenarti più a lungo con
-            il corso standard di 40 ore (6 mesi)
-          </li>
-        </ul>
-      </section>
-
-      <div className="w-full bg-black py-12">
+      <div className="w-full bg-[#1c3f60]  py-12">
         <div className="max-w-5xl mx-auto">
           <CallToAction />
         </div>
