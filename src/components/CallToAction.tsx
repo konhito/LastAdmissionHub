d6189304067bae8c831c7a574200a6e0f398d6ed
konhito <konhito02@gmail.com>
2025-04-30 22:47:23 +0530

    changed the call to acition

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -5,7 +5,7 @@ const CallToAction = () => {
   return (
     <section className="py-8">
       <div className="container mx-auto px-4 text-center">
-        <h2 className="text-5xl font-bold mb-6 text-white">
+        <h2 className="text-3xl sm:text-2xl md:text-5xl font-bold mb-6 text-white">
           Fai il primo passo verso i tuoi obiettivi
         </h2>
         <p className="max-w-2xl mx-auto mb-8 text-lg text-gray-200">
@@ -15,7 +15,7 @@ const CallToAction = () => {
         <Link
           href="https://outlook.office.com/book/CallConoscitiva@theadmissionhub.com/"
           target="_blank"
-          className="bg-[#d9c498] text-[#1c3f60] text-2xl font-bold py-4 px-16 rounded-lg transition-all inline-block hover:scale-105"
+          className="bg-[#d9c498] text-[#1c3f60] text-2xl font-bold py-4 mt-3 px-16 rounded-lg transition-all inline-block hover:scale-105"
         >
           PRENOTA UNA CALL GRATUITA
         </Link>
